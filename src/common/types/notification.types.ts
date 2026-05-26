1a33b7f2142b20e03df24584ae68c9dcf15fdba3
Begench <begenchborjakov0@gmail.com>
2026-05-26 21:17:01 +0300

    Add common notification types

---
+++ src/common/types/notification.types.ts
@@ -0,0 +1,19 @@
+export enum NotificationType {
+  MARKETING = 'marketing',
+  TRANSACTIONAL = 'transactional',
+  SECURITY = 'security',
+}
+
+export enum Channel {
+  EMAIL = 'email',
+  SMS = 'sms',
+  PUSH = 'push',
+  MESSENGER = 'messenger',
+}
+
+export enum Region {
+  GLOBAL = 'GLOBAL',
+  EU = 'EU',
+  US = 'US',
+  TR = 'TR',
+}
