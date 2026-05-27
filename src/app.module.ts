48529ab136926d2b915db87143c8446dfe49c065
Begench <begenchborjakov0@gmail.com>
2026-05-27 15:05:00 +0300

    Register policies module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './common/config/config.module';
+import { PoliciesModule } from './modules/policies/policies.module';
 import { PreferencesModule } from './modules/preferences/preferences.module';
 
 @Module({
-  imports: [AppConfigModule, PreferencesModule],
+  imports: [AppConfigModule, PreferencesModule, PoliciesModule],
 })
 export class AppModule {}
